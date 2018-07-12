12a015f55f1ae4dd526e8f789106ffe1b342a4e4
Gabrielle Finear <gabrielle.finear@nordstrom.com>
2018-07-12 11:23:41 -0700

    Lesson 8: Loaded in React, ReactDOM, and a script I created. Made a tempalte, an element, and rendered using ReactDOM.render();

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+console.log("App.js is running!")
+
+//JSX - JavaScript XML
+// var template = <p> This is JSX from app.js!</p>;
+var template = React.createElement(
+    "p",
+    { id: "someid" },
+    "This is JSX from app.js!"
+  );
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
